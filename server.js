0aad75c49d3c632db4c3e08b393eeff96b7a12ab
aseya <daga.oppo@mail.ru>
2023-10-12 17:44:17 +0300

    handlebars

--- server.js
+++ server.js
@@ -1,62 +1,60 @@
-const express = require("express")
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const static = path.resolve(__dirname, 'static')
-const dictionary = path.resolve(__dirname, 'public', 'slovar', 'dictionary.html')
-const jisla = path.resolve(__dirname, 'public', 'slovar', 'jisla.html')
-const ludi = path.resolve(__dirname, 'public', 'slovar', 'ludi.html')
-const semya = path.resolve(__dirname, 'public', 'slovar', 'semya.html')
-const juvstva = path.resolve(__dirname, 'public', 'slovar', 'juvstva.html')
-const eda = path.resolve(__dirname, 'public', 'slovar', 'eda.html')
+const static = path.resolve(__dirname, "static");
+const ludi = path.resolve(__dirname, "public", "slovar", "ludi.html");
+const semya = path.resolve(__dirname, "public", "slovar", "semya.html");
+const juvstva = path.resolve(__dirname, "public", "slovar", "juvstva.html");
+const eda = path.resolve(__dirname, "public", "slovar", "eda.html");
 
-const app = express()
-app.use(express.static(static))
-const PORT = 3000
+const app = express();
+app.use(express.static(static));
+const PORT = 3000;
 
 const handlebars = require("express-handlebars");
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
 
-app.get("/", function(request, response){
-    response.render('home/index');
-})
+app.get("/", function (request, response) {
+  response.render("home/index");
+});
 
-app.get("/information", function(request, response){
-    response.render('information');
-})
+app.get("/information", function (request, response) {
+  response.render("information");
+});
 
-app.get("/uroki", function(request,response){
-    response.render('uroki');
-})
+app.get("/uroki", function (request, response) {
+  response.render("uroki");
+});
 
-app.get("/cartinka", function(request,response){
-    response.render('cartinka');
-})
+app.get("/cartinka", function (request, response) {
+  response.render("cartinka");
+});
 
-app.get("/dictionary", function(request,response){
-    response.sendFile(dictionary);
-})
+app.get("/dictionary", function (request, response) {
+  response.render("slovar/dictionary");
+});
 
-app.get("/jisla", function(request,response){
-    response.sendFile(jisla);
-})
+app.get("/jisla", function (request, response) {
+  response.render("slovar/jisla");
+});
 
-app.get("/ludi", function(request,response){
-    response.sendFile(ludi);
-})
+app.get("/ludi", function (request, response) {
+  response.sendFile(ludi);
+});
 
-app.get("/semya", function(request,response){
-    response.sendFile(semya);
-})
+app.get("/semya", function (request, response) {
+  response.sendFile(semya);
+});
 
-app.get("/juvstva", function(request,response){
-    response.sendFile(juvstva);
-})
+app.get("/juvstva", function (request, response) {
+  response.sendFile(juvstva);
+});
 
-app.get("/eda", function(request,response){
-    response.sendFile(eda);
-})
+app.get("/eda", function (request, response) {
+  response.sendFile(eda);
+});
 
-app.listen(PORT, function() {
-    console.log("чтобы закрыть нажмите ctrl + c");
-})
+app.listen(PORT, function () {
+  console.log("чтобы закрыть нажмите ctrl + c");
+});
